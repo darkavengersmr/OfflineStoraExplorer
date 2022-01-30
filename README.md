c3ac70252349308ed20c9c7f16f69cce6d109d13
@darkavengersmr <mailsea@yandex.ru>
2022-01-30 16:26:22 +0400

    add swagger docs

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ DB: MongoDB
 - API для SPA-frontend с использованием aiohttp
 - модуль взаимодействия с MongoDB с использованием motor_asyncio
 - консольная утилита ofe_cli для создания, удаления сетевых ресурсов и обновления данных по ним в базе данных
+- документация к API в Swagger
+
+![alt text](screenshots/screenshot1.jpg "OfflineStoraExplorer")
--- http_module.py
+++ http_module.py
@@ -7,6 +7,7 @@ import motor.motor_asyncio
 from bson.objectid import ObjectId
 
 from aiohttp import web
+from aiohttp_swagger3 import SwaggerDocs, SwaggerUiSettings
 
 from db_module import get_list_dir, do_find_one, do_search
 
@@ -27,8 +28,48 @@ def setup_db(db_socket):
         logging.info('!!! Hook enable: Aiohttp and Motor in the same event loop')
 
 
-@router.get("/files/")
 async def go_to_dir(request: web.Request) -> web.Response:
+    """
+        Endpoint: files
+        ---
+        summary: Перейти в директорию по ее id или перейти в директорию выше уровнем от указанной по id
+        tags:
+          - files
+        parameters:
+          - name: operation
+            in: query
+            required: true
+            description: Допустимые команды - dir_list, dir_up
+            schema:
+              type: string
+              format: string
+          - name: id
+            in: query
+            required: true
+            description: Допустимые команды - id директории (или None для выхода на уровень ресурсов)
+            schema:
+              type: string
+              format: string
+        responses:
+          '200':
+            description: Содержимое указанной директории/ресурса
+            content:
+              application/json:
+                schema:
+                  $ref: "#/components/schemas/command"
+          '204':
+            description: Ошибка построения содержимого диретории
+            content:
+              application/json:
+                schema:
+                  $ref: "#/components/schemas/command"
+          '400':
+            description: Некорректная команда, не входит в список разрешенных
+            content:
+              application/json:
+                schema:
+                  $ref: "#/components/schemas/command"
+        """
     setup_db(db_socket)
     status = 200
 
@@ -61,8 +102,41 @@ async def go_to_dir(request: web.Request) -> web.Response:
     return web.json_response(result, status=status)
 
 
-@router.get("/search/")
 async def go_search(request: web.Request) -> web.Response:
+    """
+    Endpoint: search
+    ---
+    summary: Поиск по файловым ресурсам
+    tags:
+      - search
+    parameters:
+      - name: search
+        in: query
+        required: true
+        description: Подстрока для поиска
+        schema:
+          type: string
+          format: string
+    responses:
+      '200':
+        description: Результаты поискового запроса
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/command"
+      '204':
+        description: Ошибка поиска
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/command"
+      '400':
+        description: Некорректный запрос
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/command"
+    """
     setup_db(db_socket)
     status = 200
     try:
@@ -78,8 +152,42 @@ async def go_search(request: web.Request) -> web.Response:
     return web.json_response(result, status=status)
 
 
-@router.get("/command/")
 async def go_command(request: web.Request) -> web.Response:
+    """
+    Endpoint: command
+    ---
+    summary: Отправка команд для включения/выключения дисков или получения их статуса
+    tags:
+      - command
+    parameters:
+      - name: command
+        in: query
+        required: true
+        description: Допустимые команды - disk, status
+        schema:
+          type: string
+          format: string
+      - name: parameter
+        in: query
+        required: true
+        description: Допустимые команды - 0,1,2,3,4,5,6
+        schema:
+          type: string
+          format: string
+    responses:
+      '200':
+        description: Команда выполнена
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/command"
+      '204':
+        description: Некорректная команда, не входит в список разрешенных
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/command"
+    """
     valid_commands = ('disk', 'status')
     valid_parameters = my_disks
 
@@ -104,10 +212,25 @@ async def root_handler(request):
 
 async def init_app() -> web.Application:
     app = web.Application(client_max_size=1024 ** 3)
+
+    swagger = SwaggerDocs(
+        app,
+        swagger_ui_settings=SwaggerUiSettings(path="/docs/"),
+        title="Документация OfflineStoraExplorer API",
+        version="1.0.0",
+        components="components.yaml"
+    )
+    swagger.add_routes([
+        web.get("/files/", go_to_dir),
+        web.get("/search/", go_search),
+        web.get("/command/", go_command),
+    ])
+
     app.add_routes(router)
     app.router.add_route('*', '/', root_handler)
     app.router.add_static('/', './web/static')
     logging.basicConfig(level=logging.DEBUG)
+
     return app
 
 
